494e4495dbfa203b44198d36b88107f7da7be3b9
sina <ksina.72@gmail.com>
2021-08-29 23:36:17 +1200

    initial version of portfolio

---
+++ colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#DFBBB1',
+
+  primary: '#373F51',
+  primaryLight: '#373F51',
+  primaryDark: '#373F51',
+
+  secondary: '#F56476',
+  secondaryLight: '#E89588',
+  secondaryDark: '#E86B58',
+};
---
+++ manifest-config.js
@@ -0,0 +1,11 @@
+const colors = require('./colors');
+
+module.exports = {
+  name: 'Alice Alsford',
+  short_name: 'Alice Alsford',
+  start_url: '/',
+  background_color: colors.background,
+  theme_color: colors.primary,
+  display: 'minimal-ui',
+  icon: 'media/icon.png',
+};
