d6bf3bb5f1677212b3a26a55193e9eb5d778dd7b
sina <ksina.72@gmail.com>
2021-08-30 01:09:34 +1200

    header update with logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { SectionLinks } from 'react-scroll-section';
 import Fade from 'react-reveal/Fade';
 import RouteLink from './RouteLink';
-import Logo from './Logo/alice.png';
+import Logo from './Logo/icon.png';
 
 const capitalize = s => s && s[0].toUpperCase() + s.slice(1);
 
