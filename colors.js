c2422001720b32b626e6a4ffa081b14f4365ef4c
sina <ksina.72@gmail.com>
2021-08-30 01:21:43 +1200

    color update

--- colors.js
+++ colors.js
@@ -2,11 +2,11 @@ module.exports = {
   background: '#FFFFFF',
   backgroundDark: '#DFBBB1',
 
-  primary: '#373F51',
-  primaryLight: '#373F51',
-  primaryDark: '#373F51',
+  primary: '#bbe1fa',
+  primaryLight: '#bbe1fa',
+  primaryDark: '#bbe1fa',
 
   secondary: '#F56476',
-  secondaryLight: '#E89588',
-  secondaryDark: '#E86B58',
+  secondaryLight: '#3282b8',
+  secondaryDark: '#0f4c75',
 };
